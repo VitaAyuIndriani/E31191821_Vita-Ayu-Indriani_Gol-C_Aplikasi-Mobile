ded3bfd1e613780c91ff7f876b4d80dfe5afc916
VitaAyuIndriani <vitaayu136@gmail.com>
2021-04-25 12:06:12 +0700

    Fragment

---
+++ Fragment/app/src/main/java/com/vitaayu/fragment/ChatFragment.java
@@ -0,0 +1,33 @@
+package com.vitaayu.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ChatFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ChatFragment extends Fragment {
+    private TextView textView;
+    public ChatFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_chat, container, false);
+        textView = view.findViewById(R.id.text);
+        textView.setText("Fragment 2");
+        return view;
+    }
+}
---
+++ Fragment/app/src/main/java/com/vitaayu/fragment/HomeFragment.java
@@ -0,0 +1,39 @@
+package com.vitaayu.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.telecom.TelecomManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link HomeFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class HomeFragment extends Fragment {
+    private TextView textView;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        //komponen
+        textView = view.findViewById(R.id.text);
+        textView.setText("Fragment 1");
+        return view;
+    }
+}
