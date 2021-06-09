e3405a44ad98d69c8c46497a63ba2873c1bcb998
VitaAyuIndriani <vitaayu136@gmail.com>
2021-06-09 13:29:59 +0700

    API Volley

---
+++ APIVolley_Vita/app/src/main/java/com/vitaayu/apivolley_vita/Util/ServerAPI.java
@@ -0,0 +1,7 @@
+package com.vitaayu.apivolley_vita.Util;
+public class ServerAPI {
+    public static final String URL_DATA = "http://192.168.0.1/crud/Mahasiswa/Api";
+    public static final String URL_INSERT = "http://192.168.0.1/crud/Mahasiswa/ApiInsert";
+    public static final String URL_DELETE = "http://192.168.0.1/crud/Mahasiswa/ApiDelete";
+    public static final String URL_UPDATE = "http://192.168.0.1/crud/Mahasiswa/ApiUpdate";
+}
